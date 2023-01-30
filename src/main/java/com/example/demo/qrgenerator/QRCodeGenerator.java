a4e05dafb41c34e7f7af18b59ccf98354c488215
JuanPatinoGomez <jjpg2001@hotmail.com>
2023-01-29 23:25:00 -0500

    Eliminación codigo no usado y redundante

--- src/main/java/com/example/demo/qrgenerator/QRCodeGenerator.java
+++ src/main/java/com/example/demo/qrgenerator/QRCodeGenerator.java
@@ -9,8 +9,6 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -30,16 +28,6 @@ public class QRCodeGenerator {
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
     }
 
-    public static byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
-
-        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
-        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
-        byte[] pngData = pngOutputStream.toByteArray();
-        return pngData;
-    }
-
     public static void generateQRSalonImage(Long idSalon, int numeroSalon, String nombreEdificio, String sedeMunicipio, int width, int height, String filePath) throws WriterException, IOException {
 
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
@@ -50,7 +38,7 @@ public class QRCodeGenerator {
         //Lugar en el que se genera la imagen
         Path path = FileSystems.getDefault().getPath(filePath + nombreimagen + ".png");
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-        System.out.println("Se genero una imagen en la ruta: \n" + path.toString());
+        System.out.println("Se genero una imagen en la ruta: \n" + path);
     }
 
     public static void generateQREdificioImage(HashMap<Long, Integer> salones, String nombreEdificio, String sedeMunicipio, int width, int height, String filePath) throws WriterException, IOException {
@@ -64,7 +52,7 @@ public class QRCodeGenerator {
             //Lugar en el que se genera la imagen
             Path path = FileSystems.getDefault().getPath(filePath + nombreimagen + ".png");
             MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-            System.out.println("Se genero una imagen en la ruta: \n" + path.toString());
+            System.out.println("Se genero una imagen en la ruta: \n" + path);
         }
     }
 
@@ -92,7 +80,7 @@ public class QRCodeGenerator {
                 //Lugar en el que se genera la imagen
                 Path path = FileSystems.getDefault().getPath(filePath + nombreimagen + ".png");
                 MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-                System.out.println("Se genero una imagen en la ruta: \n" + path.toString());
+                System.out.println("Se genero una imagen en la ruta: \n" + path);
             }
         }
     }
